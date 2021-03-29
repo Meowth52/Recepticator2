5aad29017f76408b3c698f5350161e58a69d23dd
Meowth52 <meowth@meowth.se>
2021-03-29 22:46:06 +0200

    mhm

---
+++ Recepticator/Models/TodoContext .cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Recepticator.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ingredient> Ingredients { get; set; }
+    }
+}
