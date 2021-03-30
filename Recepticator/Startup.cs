ba92bbce9a33e5841e092f6cbe71eb6d53a7300c
Meowth52 <meowth@meowth.se>
2021-03-30 19:55:50 +0200

    men swagger då

--- Recepticator/Startup.cs
+++ Recepticator/Startup.cs
@@ -31,10 +31,10 @@ namespace Recepticator
             services.AddDbContext<TodoContext>(opt =>
                                    opt.UseInMemoryDatabase("Ingedients"));
             services.AddControllers();
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Recepticator", Version = "v1" });
-            //});
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Recepticator", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,8 +43,8 @@ namespace Recepticator
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Recepticator v1"));
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Recepticator v1"));
             }
 
             app.UseHttpsRedirection();
